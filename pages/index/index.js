02926495f5fca9a01e2b66121abac5f71a88e5eb
SHINING <jack7770126@126.com>
2017-06-29 22:11:41 +0800

    [new function] finish the index page.

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,24 +3,64 @@
 var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    userInfo: {},
+    checkboxs: [
+      {
+        id: 0,
+        name: '名医主检',
+        en: 'mingyi',
+        checked: false
+      },
+      {
+        id: 1,
+        name: '心脑血管',
+        en: 'xinnao',
+        checked: false
+      },
+      {
+        id: 2,
+        name: '肺部检查',
+        en: 'feibu',
+        checked: false
+      },
+      {
+        id: 3,
+        name: '肠胃检查',
+        en: 'changwei',
+        checked: false
+      },
+      {
+        id: 4,
+        name: '肿瘤早筛',
+        en: 'zhongliu',
+        checked: false
+      }
+    ]
   },
   onLoad: function () {
     console.log('onLoad')
     var that = this
     //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
+    app.getUserInfo(function (userInfo) {
       //更新数据
       that.setData({
-        userInfo:userInfo
+        userInfo: userInfo
       })
     })
+  },
+
+  //checkbox
+  checkbox(e) {
+    const that = this
+    const index = e.currentTarget.dataset.index
+    let temp = 'checkboxs[' + index + '].checked'
+    that.setData({
+      [temp]: !that.data.checkboxs[index].checked
+    })
+  },
+
+  //提交
+  postData(e) {
+    console.log(e.detail.value)
   }
 })
